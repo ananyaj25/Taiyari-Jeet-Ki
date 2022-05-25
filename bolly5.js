7705c389c4f35023697ab72ea75bb258b52e3fe4
Ananya <shavijain2125@gmail.com>
2022-05-26 03:40:44 +0530

    bollyjolly3

--- bolly5.js
+++ bolly5.js
@@ -4,8 +4,28 @@ for (let i = 0; i < counter; i++) {
   hrElement = document.createElement("HR");
   
     hrElement.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-    hrElement.style.animationDuration = 0.2 + Math.random() * 0.3 + "s";
-    hrElement.style.animationDelay = Math.random() * 5 + "s";
+    hrElement.style.animationDuration = 0.2 + Math.random() * 0.6 + "s";
+    hrElement.style.animationDelay = Math.random() * 10 + "s";
  
   document.body.appendChild(hrElement);
 }
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
