e671a5f84e16c49f8197eac6aac9c798496c9f3f
Ananya <shavijain2125@gmail.com>
2022-05-26 17:00:52 +0530

    BollywoodFinalEDIT

--- bolly5.js
+++ bolly5.js
@@ -1,17 +1,3 @@
-let hrElement;
-let counter = 100;
-for (let i = 0; i < counter; i++) {
-  hrElement = document.createElement("HR");
-  
-    hrElement.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-    hrElement.style.animationDuration = 0.2 + Math.random() * 0.6 + "s";
-    hrElement.style.animationDelay = Math.random() * 10 + "s";
- 
-  document.body.appendChild(hrElement);
-}
-
-
-
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
